74f2fc6207babfd27d563f3ac4fbe1b172e4ab79
ecyht2 <ecyht2@nottingham.edu.my>
2023-10-19 10:04:05 +0800

    Update code for F411

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 use defmt_rtt as _;
 use panic_halt as _;
 
-use stm32l4xx_hal::prelude::*;
+use stm32f4xx_hal::prelude::*;
 
 use rand::SeedableRng;
 use rand::{rngs::SmallRng, Rng};
@@ -13,7 +13,7 @@ use rand::{rngs::SmallRng, Rng};
 fn main() -> ! {
     // Setting Up Peripherals
     let cp = cortex_m::Peripherals::take().unwrap();
-    let dp = stm32l4xx_hal::pac::Peripherals::take().unwrap();
+    let dp = stm32f4xx_hal::pac::Peripherals::take().unwrap();
 
     loop {}
 }
